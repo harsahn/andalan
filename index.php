b40503a2a33543f8406815bd81d915f74fba1cbc
harsahn <htp097@gmail.com>
2022-02-21 12:56:39 +0800

    ubah style

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Lobster&family=Roboto:wght@500&display=swap" rel="stylesheet">
   <!-- ICON -->
   <link rel="stylesheet" href="assets/icon/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="assets/icon/icofont/css/icofont.css">
